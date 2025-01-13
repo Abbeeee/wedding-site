95a66a042150eb6115347ce84669771dcb9c9002
Albin Lagerquist <lagerquist.albin@gmail.com>
2025-01-13 22:49:30 +0100

    Added splide slider

--- astro-app/src/utils/sanity.ts
+++ astro-app/src/utils/sanity.ts
@@ -1,51 +1,49 @@
-import { sanityClient } from "sanity:client";
-import type { PortableTextBlock } from "@portabletext/types";
-import type { ImageAsset, Slug } from "@sanity/types";
-import groq from "groq";
+import { sanityClient } from 'sanity:client';
+import type { PortableTextBlock } from '@portabletext/types';
+import type { ImageAsset, Slug } from '@sanity/types';
+import groq from 'groq';
 
 export async function getPosts(): Promise<Post[]> {
-  return await sanityClient.fetch(
-    groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`,
-  );
+	return await sanityClient.fetch(groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`);
 }
 
 export async function getPost(slug: string): Promise<Post> {
-  return await sanityClient.fetch(
-    groq`*[_type == "post" && slug.current == $slug][0]`,
-    {
-      slug,
-    },
-  );
+	return await sanityClient.fetch(groq`*[_type == "post" && slug.current == $slug][0]`, {
+		slug
+	});
 }
 
 export async function getStartPage(): Promise<StartPage> {
-  return await sanityClient.fetch(groq`*[_type == "startPage"][0]`);
+	return await sanityClient.fetch(groq`*[_type == "startPage"][0]`);
 }
 
 export interface Post {
-  _type: "post";
-  _createdAt: string;
-  title?: string;
-  slug: Slug;
-  excerpt?: string;
-  mainImage?: ImageAsset & { alt?: string };
-  body: PortableTextBlock[];
+	_type: 'post';
+	_createdAt: string;
+	title?: string;
+	slug: Slug;
+	excerpt?: string;
+	mainImage?: ImageAsset & { alt?: string };
+	body: PortableTextBlock[];
 }
 
 export interface StartPage {
-  _type: "startPage";
-  _createdAt: string;
-  title?: string;
-  subtitle?: string;
-  heroImage?: ImageAsset & { alt?: string };
-  callToAction?: {
-    text: string;
-    link: string;
-  };
-  body: PortableTextBlock[];
-  features?: {
-    title: string;
-    description: string;
-    icon: ImageAsset & { alt?: string };
-  }[];
+	_type: 'startPage';
+	_createdAt: string;
+	title?: string;
+	subtitle?: string;
+	heroImage?: ImageAsset & { alt?: string };
+	heroSlider?: {
+		images: ImageAsset[];
+	};
+	callToAction?: {
+		text: string;
+		link: string;
+	};
+	body: PortableTextBlock[];
+	features?: {
+		title: string;
+		description: string;
+		icon: ImageAsset & { alt?: string };
+	}[];
 }
