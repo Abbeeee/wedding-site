de76be9f3c4fbaceeea0665e062d236ffedefa1e
Albin Lagerquist <lagerquist.albin@gmail.com>
2025-01-25 10:24:38 +0100

    Undo some härj

--- astro-app/src/components/ImageBlock.tsx
+++ astro-app/src/components/ImageBlock.tsx
@@ -10,19 +10,17 @@ const ImageBlock = (props: Props) => {
 	const image = props.block.image;
 
 	return (
-		<section className="imageblock bg-white py-8 md:py-12">
-			<div className="mx-auto max-w-container">
-				{props.block.heading && (
-					<h2 className="mx-auto mt-0 w-fit text-balance text-4xl md:text-5xl">{props.block.heading}</h2>
-				)}
-				{image && (
-					<img
-						className="w-full object-cover"
-						src={urlFor(image).width(1024).height(420).url()}
-						alt={(image.alt as string) || ''}
-					/>
-				)}
-			</div>
+		<section className="imageblock mx-auto max-w-container">
+			{props.block.heading && (
+				<h2 className="mx-auto mt-0 w-fit text-balance text-4xl md:text-5xl">{props.block.heading}</h2>
+			)}
+			{image && (
+				<img
+					className="w-full object-cover"
+					src={urlFor(image).width(1024).height(420).url()}
+					alt={(image.alt as string) || ''}
+				/>
+			)}
 		</section>
 	);
 };
